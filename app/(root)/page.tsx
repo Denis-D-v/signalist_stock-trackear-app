ead8c0312f6400feea08bc470e0bf19f25bd3880
EnshteinDe <sonikovskiy@gmail.com>
2026-02-27 19:15:24 +0300

    Header base  finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,9 @@ import { Button } from '@/components/ui/button';
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>Click me</Button>
+    <div className="flex justify-center items-center home-wrapper">
+      {/* <Button>Click me</Button> */}
+      Home
     </div>
   );
 };
