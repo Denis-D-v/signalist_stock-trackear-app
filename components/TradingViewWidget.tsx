beef60430deb7ea68c64929ccdb4e532c5681246
EnshteinDe <sonikovskiy@gmail.com>
2026-04-15 20:13:06 +0300

    Auth UI and Layout

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -22,17 +22,9 @@ const TradingViewWidget = ({
 
   return (
     <div className="w-full">
-      {title && (
-        <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>
-      )}
-      <div
-        className={cn('tradingview-widget-container', className)}
-        ref={containerRef}
-      >
-        <div
-          className="tradingview-widget-container__widget"
-          style={{ height, width: '100%' }}
-        />
+      {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
+      <div className={cn('tradingview-widget-container', className)} ref={containerRef}>
+        <div className="tradingview-widget-container__widget" style={{ height, width: '100%' }} />
       </div>
     </div>
   );
